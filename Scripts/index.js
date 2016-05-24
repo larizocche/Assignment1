b0dfffb2462893085179dab4e98a907c65c1dddc
larizocche <lari.zocche@gmail.com>
2016-05-24 12:49:02 -0400

    Insert grid system

--- Scripts/index.js
+++ Scripts/index.js
@@ -18,8 +18,16 @@ function writeToIntroduction(){
         console.log ("Executing writeToIntroduction");
         
         var introduction = document.getElementById("introduction");
-        introduction.textContent = "\nI am an enthusiastic graduate of Biology. I have significant work expertise through internships, a Masters in Biotechnology and a volunteer position in a research lab." +
-        "My past experiences as a Biology Teacher (delivering lectures, preparing differentiated lessons), Sales Representative (preparing sales presentations that explained product specifications and applications) and in a restaurant I developed my verbal and written skills, taught me about managing multiple priorities and how to multi task in a fast paced environment." + "n\In my most recent position as Quality Control Technician I have learned how to maintain accuracy in a high volume and repetitive work environment that required strong attention to details and where I have explored my ability for versatility and flexibility. I am highly motivated, patient and enthusiastic, always willing to acquire new knowledge and willing to learn with  people with different opinions and backgrounds.\n" +  "n\My next goal is to graduate as a Software Engineer Technician and find a good job in the IT industry. \n" ;
+        introduction.textContent = "I am an enthusiastic graduate of Biology. I have significant work expertise through internships, a Masters in Biotechnology and a volunteer position in a research lab."
+        
+        var experience = document.getElementById("experience");
+        experience.textContent =  "My past experiences as a Biology Teacher (delivering lectures, preparing differentiated lessons), Sales Representative (preparing sales presentations that explained product specifications and applications) and in a restaurant I developed my verbal and written skills, taught me about managing multiple priorities and how to multi task in a fast paced environment." + " In my most recent position as Quality Control Technician I have learned how to maintain accuracy in a high volume and repetitive work environment that required strong attention to details and where I have explored my ability for versatility and flexibility."
+        
+        var skills = document.getElementById("skills");
+        skills.textContent = "I am highly motivated, patient, always willing to acquire new knowledge and willing to learn with  people with different opinions and backgrounds."
+        
+        var objectives = document.getElementById("objectives");
+        objectives.textContent = "My next goal is to graduate as a Software Engineer Technician and find a good job in the IT industry."
     }
     writeToIntroduction();
     
--- Scripts/project.js
+++ Scripts/project.js
@@ -16,8 +16,20 @@
         console.log ("Executing writeImageDescription");
         
         var researchDescription = document.getElementById("researchDescription");
-        researchDescription.textContent = "\nResearch paper description here\n" ;
+        researchDescription.textContent = "\nDevelopment and characterization of a new inbred transgenic rat strain expressing DsRed monomeric fluorescent protein.\n" +
+        "Authors: Montanari, S.; Wang, X-H.; Yannarelli, G.; Dayan, V.; Berger, T.; Zocche, L.; Kobayashi, E.;  Viswanathan, S.; Keating, A."
+        
+         var researchDescription2 = document.getElementById("researchDescription2");
+        researchDescription2.textContent = "\nPap Test as the First Step in Screening Genetic Stability in Cell-Based Therapy.\n" +
+        "Authors: Carvalho, K.A.T.; Zocche, Larissa, Irioda, A.C., Souza, C., Ferreira, R.J.; Aliprandini, E.; Cunha, R.C.; Francisco, J.C.; Guarita-Souza, L.C.; Malvezzi, M.; Beltrame, M.; Mesquita, L.; Kuczera, D.; Vargas, R."
+
+        var researchDescription3 = document.getElementById("researchDescription3");
+        researchDescription3.textContent = "\Functional Outcome of Bone Marrow Stem Cells (CD45+/CD34−) After Cell Therapy in Acute Spinal Cord Injury: In Exercise Training and in Sedentary Rats. \n" +
+        
+        "Authors: Carvalho, K. A. T.; Cunha, R. C.; Vialle E.N; Osiecki, R.; Moreira, GHG; Simeoni, R. B.; Francisco, J. C.; Guarita-Souza, L. C.; Oliveira, L.; Zocche, Larissa; Olandoski, M."  
     }
     writeImageDescription();
+    
+    
         
 }());
