36d0615d1c40bf90f9d4c077d4e1128f3dec56ef
larizocche <lari.zocche@gmail.com>
2016-05-30 16:25:36 -0400

    index and project page updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,12 +11,9 @@
     var highlights1 = document.getElementById("highlights1");
     var highlights2 = document.getElementById("highlights2");
     var highlights3 = document.getElementById("highlights3");
-    var education1 = document.getElementById("education1");
-    var education1A = document.getElementById("education1A");
-    var education2 = document.getElementById("education2");
-    var education2A = document.getElementById("education2A");
-    var education3 = document.getElementById("education3");
-    var education3A = document.getElementById("education3A");
+    var education = document.getElementById("education");
+    var experience = document.getElementById("experience");
+ 
 
     //projects
     var researchDescription = document.getElementById("researchDescription");
@@ -30,19 +27,16 @@
     paragraphs[0] = "Methodical and strong organizational skills";
     paragraphs[1] = "Excellent attention to details";
     paragraphs[2] = "Highly motivated, patient, always willing to acquire new knowledge and willing to learn with  people with different opinions and backgrounds.";
-    paragraphs[3] = "B. S.c in Biological Sciences";
-    paragraphs[4] = "Pontifical Catholic University of Paraná, Brazil.";
-    paragraphs[5] = "Master’s in Biotechnology";
-    paragraphs[6] = "Pequeno Príncipe University, Brazil";
-    paragraphs[7] = "Software Engineering Technician";
-    paragraphs[8] = "Centennial College, Toronto, Canada";
+    paragraphs[3] = "B. S.c in Biological Sciences <br> Pontifical Catholic University of Paraná, Curitiba, Brazil <br> Master’s in Biotechnology <br> Pequeno Príncipe University, Curitiba, Brazil <br> Software Engineering Technician <br> Centennial College, Toronto, Canada";
+    paragraphs[4] = "Quality Control Technician <br> Baylis Medical Company, Mississauga, Canada <br> Research Assistant Volunteer <br> Cell Therapy Translational Research Laboratory (Dr Armand Keating Lab), Princess Margaret Hospital, Toronto, Canada <br> Biology Teacher <br> Estadual Do Parana High School, Curitiba, Brazil";
+    
 
     
     
     //data for the projects page
-    paragraphs[9] = "Development and characterization of a new inbred transgenic rat strain expressing DsRed monomeric fluorescent protein. Authors: Montanari, S.; Wang, X-H.; Yannarelli, G.; Dayan, V.; Berger, T.; Zocche, L.; Kobayashi, E.;  Viswanathan, S.; Keating, A.";
-    paragraphs[10] = "Pap Test as the First Step in Screening Genetic Stability in Cell-Based Therapy. Authors: Carvalho, K.A.T.; Zocche, Larissa, Irioda, A.C., Souza, C., Ferreira, R.J.; Aliprandini, E.; Cunha, R.C.; Francisco, J.C.; Guarita-Souza, L.C.; Malvezzi, M.; Beltrame, M.; Mesquita, L.; Kuczera, D.; Vargas, R.";
-    paragraphs[11] = "Functional Outcome of Bone Marrow Stem Cells (CD45+/CD34−) After Cell Therapy in Acute Spinal Cord Injury: In Exercise Training and in Sedentary Rats.;Authors: Carvalho, K. A. T.; Cunha, R. C.; Vialle E.N; Osiecki, R.; Moreira, GHG; Simeoni, R. B.; Francisco, J. C.; Guarita-Souza, L. C.; Oliveira, L.; Zocche, Larissa; Olandoski, M.";
+    paragraphs[5] = "Development and characterization of a new inbred transgenic rat strain expressing DsRed monomeric fluorescent protein.<br> Authors: Montanari, S.; Wang, X-H.; Yannarelli, G.; Dayan, V.; Berger, T.; Zocche, L.; Kobayashi, E.;  Viswanathan, S.; Keating, A.";
+    paragraphs[6] = "Pap Test as the First Step in Screening Genetic Stability in Cell-Based Therapy.<br> Authors: Carvalho, K.A.T.; Zocche, Larissa, Irioda, A.C., Souza, C., Ferreira, R.J.; Aliprandini, E.; Cunha, R.C.; Francisco, J.C.; Guarita-Souza, L.C.; Malvezzi, M.; Beltrame, M.; Mesquita, L.; Kuczera, D.; Vargas, R.";
+    paragraphs[7] = "Functional Outcome of Bone Marrow Stem Cells (CD45+/CD34−) After Cell Therapy in Acute Spinal Cord Injury: In Exercise Training and in Sedentary Rats. <br> Authors: Carvalho, K. A. T.; Cunha, R. C.; Vialle E.N; Osiecki, R.; Moreira, GHG; Simeoni, R. B.; Francisco, J. C.; Guarita-Souza, L. C.; Oliveira, L.; Zocche, Larissa; Olandoski, M.";
 
     //check if the paragraphs exists
     //Index page
@@ -55,33 +49,22 @@
     if (highlights3) {
         highlights3.textContent = paragraphs[2]
     }
-    if (education1) {
-        education1.textContent = paragraphs[3]
+    if (education) {
+        education.textContent = paragraphs[3]
     }
-    if (education1A) {
-        education1A.textContent = paragraphs[4]
-    }
-    if (education2) {
-        education2.textContent = paragraphs[5]
-    }
-    if (education2A) {
-        education2A.textContent = paragraphs[6]
-    }
-     if (education3) {
-        education3.textContent = paragraphs[7]
-    }
-    if (education3A) {
-        education3A.textContent = paragraphs[8]
+    
+    if (experience) {
+        experience.textContent = paragraphs[4]
     }
     //PROJECTS PAGE
     if (researchDescription) {
-        researchDescription.textContent = paragraphs[9]
+        researchDescription.textContent = paragraphs[5]
     }
     if (researchDescription2) {
-        researchDescription2.textContent = paragraphs[10]
+        researchDescription2.textContent = paragraphs[6]
     }
     if (researchDescription3) {
-        researchDescription3.textContent = paragraphs[11]
+        researchDescription3.textContent = paragraphs[7]
     }
 
 //if a form field is empty, this function shows a message and prevent the form from being submitted.
